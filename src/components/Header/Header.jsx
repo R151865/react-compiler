687122b50f815a9e37b8c8e436cc97c24d40064c
sb-react-dev <sulthan.babu@jukshio.com>
2024-10-04 18:47:30 +0530

    pushind code

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import { Box, HStack, Text } from "@chakra-ui/react";
+
+
+const Header = () => {
+
+  return (
+    <Box>
+    <Text mb={2} fontSize="lg"
+    color="white"
+    letterSpacing="5px"
+    fontWeight={700}
+    
+    >
+          COMPILO
+      </Text>
+            
+    </Box>
+  );
+};
+export default Header;
